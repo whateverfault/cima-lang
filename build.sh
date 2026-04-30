a3607b13472e9af4e4015817e6da778fb0ba7880
k0n4eniyD0lbo3b <slowmanart@gmail.com>
2026-04-30 10:55:17 +0300

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 mkdir -p bin
 
-cc -std=c11 -o ./bin/cima ./src/main.c ./src/lexer/lexer.c ./src/parser/parser.c ./src/executor/executor.c ./src/executor/funcs.c ./src/types/type.c ./src/other/console/console.c -I./src/thirdparty -I./src/
+cc -Wno-incompatible-pointer-types -std=c11 -o ./bin/cima ./src/main.c ./src/lexer/lexer.c ./src/parser/parser.c ./src/executor/executor.c ./src/executor/funcs/funcs.c ./src/executor/types/type.c ./src/other/sys/sys.c ./src/other/built_in.c -I./src/thirdparty -I./src/
+
